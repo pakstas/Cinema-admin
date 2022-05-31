f8f86b2def390f364f4725a1d4d805d0a97e21ed
Pakstas <pakstas@gmail.com>
2022-06-01 02:36:29 +0300

    added color palettes to charts

--- src/utils/ChartsData.js
+++ src/utils/ChartsData.js
@@ -1,3 +1,105 @@
+function ChartsColorPalette(index) {
+  switch (index) {
+    case 1:
+      return [
+        "#fd7f6f",
+        "#7eb0d5",
+        "#b2e061",
+        "#bd7ebe",
+        "#ffb55a",
+        "#ffee65",
+        "#beb9db",
+        "#fdcce5",
+        "#8bd3c7",
+      ];
+    case 2:
+      return [
+        "#e60049",
+        "#0bb4ff",
+        "#50e991",
+        "#e6d800",
+        "#9b19f5",
+        "#ffa300",
+        "#dc0ab4",
+        "#b3d4ff",
+        "#00bfa0",
+      ];
+    case 3:
+      return [
+        "#1984c5",
+        "#22a7f0",
+        "#63bff0",
+        "#a7d5ed",
+        "#e2e2e2",
+        "#e1a692",
+        "#de6e56",
+        "#e14b31",
+        "#c23728",
+      ];
+    case 4:
+      return [
+        "#ffb400",
+        "#d2980d",
+        "#a57c1b",
+        "#786028",
+        "#363445",
+        "#48446e",
+        "#5e569b",
+        "#776bcd",
+        "#9080ff",
+      ];
+    case 5:
+      return [
+        "#54bebe",
+        "#76c8c8",
+        "#98d1d1",
+        "#badbdb",
+        "#dedad2",
+        "#e4bcad",
+        "#df979e",
+        "#d7658b",
+        "#c80064",
+      ];
+    case 6:
+      return [
+        "#e27c7c",
+        "#a86464",
+        "#6d4b4b",
+        "#503f3f",
+        "#333333",
+        "#3c4e4b",
+        "#466964",
+        "#599e94",
+        "#6cd4c5",
+      ];
+    case 7:
+      return [
+        "#ea5545",
+        "#f46a9b",
+        "#ef9b20",
+        "#edbf33",
+        "#ede15b",
+        "#bdcf32",
+        "#87bc45",
+        "#27aeef",
+        "#b33dc6",
+      ];
+    default:
+      return [
+        "#ffb400",
+        "#9080ff",
+        "#00C89B",
+        "#8F131E",
+        "#00A5FA",
+        "#b37d00",
+        "#3F3B96",
+        "#00876D",
+        "#DA3F41",
+        "#00D5F5",
+      ];
+  }
+}
+
 const ChartsData = {
   areachart: {
     series: [
@@ -11,6 +113,7 @@ const ChartsData = {
       },
     ],
     options: {
+      colors: ChartsColorPalette(),
       grid: {
         show: false,
       },
@@ -73,6 +176,7 @@ const ChartsData = {
   },
   barchart: {
     options: {
+      colors: ChartsColorPalette(),
       grid: {
         show: true,
         borderColor: "transparent",
@@ -150,6 +254,7 @@ const ChartsData = {
   radialchart: {
     series: [44, 55, 67, 83],
     options: {
+      colors: ChartsColorPalette(),
       chart: {
         height: 350,
         type: "radialBar",
