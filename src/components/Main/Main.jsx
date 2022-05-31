683cb317934315992ccf28d0daab1e1134838a9e
Pakstas <pakstas@gmail.com>
2022-06-01 02:35:23 +0300

    fixed main layout width

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -3,7 +3,14 @@ import { Box, Toolbar } from "@mui/material";
 
 function Main({ children }) {
   return (
-    <Box sx={{ flexGrow: 1, maxWidth: "1400px", py: 2, pr: { lg: 2 } }}>
+    <Box
+      sx={{
+        flexGrow: 1,
+        maxWidth: { lg: "1400px", xs: "100%" },
+        py: 2,
+        pr: { lg: 2 },
+      }}
+    >
       <Toolbar sx={{ minHeight: 64 }} />
       {children}
     </Box>
